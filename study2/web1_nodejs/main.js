4e4702642d9381c8af5c0c5361fd0665544b709b
sisparang <goflvhxj2547@gmail.com>
2020-03-01 14:07:30 +0900

    홈페이지 구현

--- study2/web1_nodejs/main.js
+++ study2/web1_nodejs/main.js
@@ -7,10 +7,11 @@ app = http.createServer(function (request, response) {
     let _url = request.url;
     let queryData = url.parse(_url, true).query;
     let pathName = url.parse(_url, true).pathname;
-    let title = queryData.id;
 
     if(pathName === '/') {
-        fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
+        if(queryData.id === undefined){
+            let title = 'Welcome';
+            let description = 'Hello, Node.js';
             let template = `
             <!doctype html>
             <html>
@@ -32,7 +33,32 @@ app = http.createServer(function (request, response) {
             `;
             response.writeHead(200);
             response.end(template);
-        });
+        } else {
+            fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
+                let title = queryData.id;
+                let template = `
+                <!doctype html>
+                <html>
+                <head>
+                  <title>WEB1 - ${title}</title>
+                  <meta charset="utf-8">
+                </head>
+                <body>
+                  <h1><a href="/">WEB</a></h1>
+                  <ul>
+                    <li><a href="/?id=HTML">HTML</a></li>
+                    <li><a href="/?id=CSS">CSS</a></li>
+                    <li><a href="/?id=JAVASCRIPT">JavaScript</a></li>
+                  </ul>
+                  <h2>${title}</h2>
+                  <p>${description}</p>
+                </body>
+                </html>
+                `;
+                response.writeHead(200);
+                response.end(template);
+            });
+        }
     } else {
         response.writeHead(404);
         response.end('Not Found');
