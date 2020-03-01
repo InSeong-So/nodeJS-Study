8ec821ecb3cae9a57b53abf1998267542821e466
sisparang <goflvhxj2547@gmail.com>
2020-03-01 19:11:31 +0900

    중복코드 리팩토링

--- study2/web1_nodejs/main.js
+++ study2/web1_nodejs/main.js
@@ -2,6 +2,34 @@ let http = require('http');
 let fs = require('fs');
 let url = require('url');
 
+function templateHTML(title, list, body){
+    return `
+                <!doctype html>
+                <html>
+                <head>
+                  <title>WEB1 - ${title}</title>
+                  <meta charset="utf-8">
+                </head>
+                <body>
+                  <h1><a href="/">WEB</a></h1>
+                    ${list}
+                    ${body}
+                </body>
+                </html>
+                `;
+}
+
+function templateList(fileList){
+    let list = '<ul>';
+    let i = 0;
+    while (i < fileList.length) {
+        list = list + `<li><a href="/?id=${fileList[i]}">${fileList[i]}</a></li>`;
+        i = i + 1;
+    }
+    list = list + '</ul>';
+    return list;
+}
+
 let app = http.createServer(function (request, response) {
     let _url = request.url;
     let queryData = url.parse(_url, true).query;
@@ -13,30 +41,7 @@ let app = http.createServer(function (request, response) {
                 console.log(fileList);
                 let title = 'Welcome';
                 let description = 'Hello, Node.js';
-                let list = '<ul>';
-                let i = 0;
-                while (i < fileList.length) {
-                    list = list + `<li><a href="/?id=${fileList[i]}">${fileList[i]}</a></li>`;
-                    i = i + 1;
-                }
-
-                list = list + '</ul>';
-
-                let template = `
-                <!doctype html>
-                <html>
-                <head>
-                  <title>WEB1 - ${title}</title>
-                  <meta charset="utf-8">
-                </head>
-                <body>
-                  <h1><a href="/">WEB</a></h1>
-                  ${list}
-                  <h2>${title}</h2>
-                  <p>${description}</p>
-                </body>
-                </html>
-                `;
+                let template = templateHTML(title, templateList(fileList), `<h2>${title}</h2>${description}`);
                 response.writeHead(200);
                 response.end(template);
             })
@@ -51,21 +56,7 @@ let app = http.createServer(function (request, response) {
                     }
                     list = list + '</ul>';
                     let title = queryData.id;
-                    let template = `
-                    <!doctype html>
-                    <html>
-                    <head>
-                      <title>WEB1 - ${title}</title>
-                      <meta charset="utf-8">
-                    </head>
-                    <body>
-                      <h1><a href="/">WEB</a></h1>
-                      ${list}
-                      <h2>${title}</h2>
-                      <p>${description}</p>
-                    </body>
-                    </html>
-                    `;
+                    let template = templateHTML(title, templateList(fileList), `<h2>${title}</h2>${description}`);
                     response.writeHead(200);
                     response.end(template);
                 });
