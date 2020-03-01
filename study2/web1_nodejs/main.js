9d7c0f2568375d1a7c82faec91daa0f0ae451e69
sisparang <goflvhxj2547@gmail.com>
2020-03-01 13:31:59 +0900

    url을 통해 입력된 값 사용하기(queryString)

--- study2/web1_nodejs/main.js
+++ study2/web1_nodejs/main.js
@@ -1,16 +1,19 @@
 let http = require('http');
 let fs = require('fs');
+let url = require('url');
+
 let app = http.createServer(function(request, response){
-    let url = request.url;
-    if(url = '/'){
-        url = '/index.html';
+    let _url = request.url;
+    let queryData = url.parse(_url, true).query;
+    if(_url == '/'){
+        _url = '/index.html';
     }
-    if(url == '/favicon.ico'){
+    if(_url == '/favicon.ico'){
         response.writeHead(404);
         response.end();
         return;
     }
     response.writeHead(200);
-    response.end(fs.readFileSync(__dirname + url));
+    response.end(queryData.id);
 })
 app.listen(8226);
