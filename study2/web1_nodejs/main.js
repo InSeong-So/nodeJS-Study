f561c5c6b5c1d883c0a3cf9ba2d7ceaf868647fd
sisparang <goflvhxj2547@gmail.com>
2020-03-01 14:01:15 +0900

    Not Found 구현

--- study2/web1_nodejs/main.js
+++ study2/web1_nodejs/main.js
@@ -2,40 +2,40 @@ let http = require('http');
 let fs = require('fs');
 let url = require('url');
 
-let app = http.createServer(function(request, response){
+let app;
+app = http.createServer(function (request, response) {
     let _url = request.url;
     let queryData = url.parse(_url, true).query;
+    let pathName = url.parse(_url, true).pathname;
     let title = queryData.id;
-    if(_url == '/'){
-        title = 'Welcome';
-    }
-    if(_url == '/favicon.ico'){
+
+    if(pathName === '/') {
+        fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
+            let template = `
+            <!doctype html>
+            <html>
+            <head>
+              <title>WEB1 - ${title}</title>
+              <meta charset="utf-8">
+            </head>
+            <body>
+              <h1><a href="/">WEB</a></h1>
+              <ul>
+                <li><a href="/?id=HTML">HTML</a></li>
+                <li><a href="/?id=CSS">CSS</a></li>
+                <li><a href="/?id=JAVASCRIPT">JavaScript</a></li>
+              </ul>
+              <h2>${title}</h2>
+              <p>${description}</p>
+            </body>
+            </html>
+            `;
+            response.writeHead(200);
+            response.end(template);
+        });
+    } else {
         response.writeHead(404);
-        response.end();
-        return;
+        response.end('Not Found');
     }
-    response.writeHead(200);
-    fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
-        let template = `
-        <!doctype html>
-    <html>
-    <head>
-      <title>WEB1 - ${title}</title>
-      <meta charset="utf-8">
-    </head>
-    <body>
-      <h1><a href="/">WEB</a></h1>
-      <ul>
-        <li><a href="/?id=HTML">HTML</a></li>
-        <li><a href="/?id=CSS">CSS</a></li>
-        <li><a href="/?id=JAVASCRIPT">JavaScript</a></li>
-      </ul>
-      <h2>${title}</h2>
-      <p>${description}</p>
-    </body>
-    </html>
-        `;
-        response.end(template);
-    });
 });
 app.listen(8226);
