418ca71acda747ea36c142046b729a7b500d1027
sisparang <goflvhxj2547@gmail.com>
2020-03-01 19:23:44 +0900

    글 생성 UI 만들기

--- study2/web1_nodejs/main.js
+++ study2/web1_nodejs/main.js
@@ -2,7 +2,7 @@ let http = require('http');
 let fs = require('fs');
 let url = require('url');
 
-function templateHTML(title, list, body){
+function templateHTML(title, list, body) {
     return `
                 <!doctype html>
                 <html>
@@ -13,13 +13,14 @@ function templateHTML(title, list, body){
                 <body>
                   <h1><a href="/">WEB</a></h1>
                     ${list}
+                    <a href="/create">create</a>
                     ${body}
                 </body>
                 </html>
                 `;
 }
 
-function templateList(fileList){
+function templateList(fileList) {
     let list = '<ul>';
     let i = 0;
     while (i < fileList.length) {
@@ -34,11 +35,9 @@ let app = http.createServer(function (request, response) {
     let _url = request.url;
     let queryData = url.parse(_url, true).query;
     let pathName = url.parse(_url, true).pathname;
-
     if (pathName === '/') {
         if (queryData.id === undefined) {
             fs.readdir('data', function (err, fileList) {
-                console.log(fileList);
                 let title = 'Welcome';
                 let description = 'Hello, Node.js';
                 let template = templateHTML(title, templateList(fileList), `<h2>${title}</h2>${description}`);
@@ -62,6 +61,19 @@ let app = http.createServer(function (request, response) {
                 });
             });
         }
+    } else if (pathName === '/create') {
+        fs.readdir('data', function (err, fileList) {
+            let title = 'WEB - create';
+            let template = templateHTML(title, templateList(fileList), `
+            <form action="http://loacalhost:8226/process_create" method="post">
+                <p><input type="text" name="title" placeholder="title"></p>
+                <p><textarea name="description" placeholder="description"></textarea></p>
+                <p><input type="submit" value="SUBMIT"></p>
+            </form>
+            `);
+            response.writeHead(200);
+            response.end(template);
+        })
     } else {
         response.writeHead(404);
         response.end('Not Found');
