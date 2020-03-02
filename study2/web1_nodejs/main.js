2e2f01803bf27b1d31d6b7fb70bc43f343ceb694
sisparang <goflvhxj2547@gmail.com>
2020-03-02 20:58:52 +0900

    글 수정 및 저장하기

--- study2/web1_nodejs/main.js
+++ study2/web1_nodejs/main.js
@@ -3,7 +3,7 @@ let fs = require('fs');
 let url = require('url');
 let qs = require('querystring');
 
-function templateHTML(title, list, body) {
+function templateHTML(title, list, body, control) {
     return `
                 <!doctype html>
                 <html>
@@ -14,7 +14,7 @@ function templateHTML(title, list, body) {
                 <body>
                   <h1><a href="/">WEB</a></h1>
                     ${list}
-                    <a href="/create">create</a>
+                    ${control}
                     ${body}
                 </body>
                 </html>
@@ -41,22 +41,22 @@ let app = http.createServer(function (request, response) {
             fs.readdir('data', function (err, fileList) {
                 let title = 'Welcome';
                 let description = 'Hello, Node.js';
-                let template = templateHTML(title, templateList(fileList), `<h2>${title}</h2>${description}`);
+                let template = templateHTML(title, templateList(fileList),
+                    `<h2>${title}</h2>${description}`,
+                    `<a href="/create">create</a>`
+                );
                 response.writeHead(200);
                 response.end(template);
             })
         } else {
-            fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
-                fs.readdir('data', function (err, fileList) {
-                    let list = '<ul>';
-                    let i = 0;
-                    while (i < fileList.length) {
-                        list = list + `<li><a href="/?id=${fileList[i]}">${fileList[i]}</a></li>`;
-                        i = i + 1;
-                    }
-                    list = list + '</ul>';
+            fs.readdir('data', function (err, fileList) {
+                fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
+                    let list = templateList(fileList);
                     let title = queryData.id;
-                    let template = templateHTML(title, templateList(fileList), `<h2>${title}</h2>${description}`);
+                    let template = templateHTML(title, templateList(fileList),
+                        `<h2>${title}</h2>${description}`,
+                        `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+                    );
                     response.writeHead(200);
                     response.end(template);
                 });
@@ -66,7 +66,7 @@ let app = http.createServer(function (request, response) {
         fs.readdir('data', function (err, fileList) {
             let title = 'WEB - create';
             let template = templateHTML(title, templateList(fileList), `
-            <form action="http://localhost:8226/process_create" method="post">
+            <form action="/process_create" method="post">
                 <p><input type="text" name="title" placeholder="title"></p>
                 <p><textarea name="description" placeholder="description"></textarea></p>
                 <p><input type="submit" value="SUBMIT"></p>
@@ -84,9 +84,44 @@ let app = http.createServer(function (request, response) {
             let post = qs.parse(body);
             let title = post.title;
             let description = post.description;
-            fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-                response.writeHead(302, {Location:`/?id=${title}`});
+            fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
+                response.writeHead(302, {Location: `/?id=${title}`});
                 response.end();
+            });
+        });
+    } else if (pathName === '/update') {
+        fs.readdir('data', function (err, fileList) {
+            fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
+                let list = templateList(fileList);
+                let title = queryData.id;
+                let template = templateHTML(title, templateList(fileList), `
+                <form action="/process_update" method="post">
+                    <input type="hidden" name="id" value="${title}">
+                    <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+                    <p><textarea name="description" placeholder="description">${description}</textarea></p>
+                    <p><input type="submit" value="SUBMIT"></p>
+                </form>`,
+                    `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+                );
+                response.writeHead(200);
+                response.end(template);
+            });
+        });
+    } else if (pathName === '/process_update') {
+        let body = '';
+        request.on('data', function (data) {
+            body += data;
+        });
+        request.on('end', function () {
+            let post = qs.parse(body);
+            let id = post.id;
+            let title = post.title;
+            let description = post.description;
+            fs.rename(`data/${id}`, `data/${title}`, function (err) {
+                fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
+                    response.writeHead(302, {Location: `/?id=${title}`});
+                    response.end();
+                });
             });
         });
     } else {
